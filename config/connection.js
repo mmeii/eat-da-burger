2379457db3eedc59fd2eda1a75c4035c63bf0e99
Mengmei Tu <redmmeii@gmail.com>
2021-01-25 16:55:22 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+const connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -11,14 +12,6 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
     })
 }
-
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "burgers_db"
-// });
 
 // Make connection.
 connection.connect(function (err) {
